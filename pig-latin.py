e51ce8f17b0fe67699babfd6e1f61155cf75b678
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-02-27 07:40:57 -0800

    coded the conditinals and loops to figure the words starting with vows and consonents

--- pig-latin.py
+++ pig-latin.py
@@ -1,11 +1,33 @@
 # GET SENTENCE FROM USER
-sentence = input('Please provide a sentence to convert: ').strip()
+sentence = input('Please provide a sentence to convert: ').strip().lower()
 
 # SPLIT SENTENCE INTO WORDS
 words = sentence.split()
 
 # LOOP THROUGH WORDS AND CONVERT THEM INTO PIG-LATIN
+new_words = []
 
+## if starts with a vowel, just add "yay"
+for word in words:
+	if word[0] in 'aeiou':
+		new_words.append(word + 'yay')
+
+	## otherwise move the first consonant to end and add 'ay'
+	else: 
+		temp_word = ''
+
+		for i in range(0, len(word)) :
+
+			if word[i] not in 'aeiou':
+				temp_word = temp_word + word[i]
+				#print('temp: ', temp_word)
+			else:
+				
+				#print('word[i]: ', word[i])
+				new_words.append(word[i:] + temp_word + 'ay')
+				break
+
+print(new_words)
 # STICK THE WORDS BACK TOGETHER INTO SENTENCE
 
 # OUTPUT THE FINAL STRING
